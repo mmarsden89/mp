4efc2ec795b35e4e432cba1101fb763bcb07c00f
mmarsden89 <mattmarsdendev@gmail.com>
2021-10-26 17:27:18 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import background from "./Styles/Assets/background.jpeg";
+import logo from "./Styles/Assets/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: `url(${background})` }}>
+      <img src={logo} className="logo" />
+      <div className="coming-soon-container">
+        <div className="coming-soon">we're building</div>
+      </div>
+      <div className="footer">
+        <div></div>
+        <div className="c"> c 2021 Marsden Properties</div>
+        <div>(123)456-7890</div>
+      </div>
     </div>
   );
 }
